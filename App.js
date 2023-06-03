febb66cb2d6ba49adc5110a4962257aeb52f5471
hajin-chung <hajinchung1@gmail.com>
2023-06-03 22:25:44 +0900

    tabbar design with dummy components

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
-import { Register } from './components/Register';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Register } from "./components/Register";
+import { TabBar } from "./components/TabBar";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  // TODO: if user is authenticated show tabs if not show login page
+  // TODO: change components in routes to the correct ones
+
   return (
-    <Register />
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+        tabBar={(props) => <TabBar {...props} />}
+      >
+        <Tab.Screen name="Home" component={Register} />
+        <Tab.Screen name="Matches" component={Register} />
+        <Tab.Screen name="Ranking" component={Register} />
+        <Tab.Screen name="User" component={Register} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ components/TabBar.js
@@ -0,0 +1,51 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, TouchableHighlight, TouchableOpacity, View } from "react-native";
+
+const tabIconMap = {
+  Home: "sports-kabaddi",
+  User: "person",
+  Matches: "format-list-bulleted",
+  Ranking: "leaderboard",
+};
+
+export function TabBar({ state, descriptors, navigation }) {
+  const handleNavigation = (routeName, index) => {
+    if (state.index !== index) {
+      navigation.navigate({ name: routeName, merge: true });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {state.routeNames.map((routeName, index) => (
+        <TouchableOpacity
+          onPress={() => handleNavigation(routeName, index)}
+          style={styles.tabButton}
+        >
+          <MaterialIcons
+            name={tabIconMap[routeName]}
+            size={20}
+            style={{
+              padding: 10,
+              color: state.index === index ? "#50a42f" : "#bbb",
+            }}
+          />
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100vw",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  tabButton: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+  },
+});
