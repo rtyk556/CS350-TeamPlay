af37138679804e02877251e5f0f8f799e1df2582
hajin-chung <hajinchung1@gmail.com>
2023-06-04 01:55:22 +0900

    rename home, matches

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Register } from "./components/Register";
-import Login from './components/Login';
 import { TabBar } from "./components/TabBar";
-import { Home } from "./components/Home";
-import { Matches } from "./components/Matches";
+import { Records } from "./components/Records";
+import { Match } from "./components/Match";
 import { Ranking } from "./components/Ranking";
 
 const Tab = createBottomTabNavigator();
@@ -20,8 +19,8 @@ export default function App() {
         initialRouteName="Home"
         tabBar={(props) => <TabBar {...props} />}
       >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Matches" component={Matches} />
+        <Tab.Screen name="Home" component={Match} />
+        <Tab.Screen name="Matches" component={Records} />
         <Tab.Screen name="Ranking" component={Ranking} />
         <Tab.Screen name="User" component={Register} />
       </Tab.Navigator>
